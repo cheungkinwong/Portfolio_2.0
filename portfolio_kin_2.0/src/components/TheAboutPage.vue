89345aa621658a2edb10b1c06c0697666aa2e0f6
cheungkinwong <cheungkin.wong@gmail.com>
2019-10-08 15:37:18 +0200

    correction in about page

--- portfolio_kin_2.0/src/components/TheAboutPage.vue
+++ portfolio_kin_2.0/src/components/TheAboutPage.vue
@@ -4,10 +4,10 @@
       <v-container class="pa-2">
         <div>
           <div class="headline mb-2">About me</div>
-          <p>Born in Vilvoorde, 28/12/1983. My parents are from Hong Kong, China. Growing up I've always been fascinated by technology and science. I spend a lot of time in the library and loved to play on the computer.</p>
-          <p>When I had to choose my studies, I chose science because I loved chemistry and doing experiments. But when I started working in a laboratory I quickly found out that studying science and doing a job in science wasn't quite the same thing. I wanted to do something more creative and more tech related and with the proper retraining started to work in the printing sector.</p>
-          <p>I really enjoyed my job as a prepress, every project came with it's own set of challenges and it allowed me to be creative at times. But after 9 years, I've noticed the printing sector going into a decline, there were less work, and the boss asked if I'd make a website with a webshop for the company to boost sales. I didn't had any experience making websites at that point but was eager to do give it a try.</p>
-          <p>It took a while and a lot of trial and error but It was functional and while I learned a lot in the process I wanted to be able to do it properly. When I saw an add from BeCode for a free retraining in Junior Web Developer I knew what my next step was going to be.</p>
+          <p>I was born in Vilvoorde, 28/12/1983. My parents are from Hong Kong, China. Growing up, I've always been fascinated by technology and science. I spent a lot of time in the library and loved to play on the computer.</p>
+          <p>When I had to choose my studies, I  went the scientific route, because I loved chemistry and doing experiments. But when I started working in a laboratory I quickly found out that studying science and doing a job in science wasn't quite the same thing. I wanted to do something more creative and more tech-related. With the proper retraining, I started to work in the printing sector.</p>
+          <p>I really enjoyed my job as a Prepress Coordinator, every project came with  its own set of challenges and it allowed me to be creative at times. But after 9 years, I noticed the printing sector going into a steady decline: there was less work, and the manager asked if I could make a website with a webshop for the company to boost sales. I didn't have  any experience making websites at that point, but was eager to give it a try.</p>
+          <p>It took a while and a lot of trial and error but  it was functional, and while I learned a lot in the process, I wanted to be able to do it properly, and perhaps even professionally. When I saw an ad from BeCode for a free training as Junior Web Developer, I instantly knew what my next step was going to be.</p>
         </div>
 
         <v-card-actions>
